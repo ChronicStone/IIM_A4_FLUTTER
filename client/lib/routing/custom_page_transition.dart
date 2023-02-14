159618e61c475eaca0c5475b0a40d13932818a60
ChronicStone <55083156+ChronicStone@users.noreply.github.com>
2023-02-14 16:10:08 +0100

    feat: refactor server struct

--- client/lib/routing/custom_page_transition.dart
+++ client/lib/routing/custom_page_transition.dart
@@ -9,7 +9,7 @@ class CustomPageTransitionBuilder extends PageTransitionsBuilder {
     Animation<double> secondaryAnimation,
     Widget child,
   ) {
-    var begin = Offset(1.0, 0.0);
+    var begin = const Offset(1.0, 0.0);
     var end = Offset.zero;
     var curve = Curves.ease;
     var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
